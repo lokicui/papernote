e74a9d792cd334cea717ab68d6713ecaf0420b92
Chenjie@lab <sonyfe25cp@gmail.com>
2014-07-17 17:27:38 +0800

    new papers about how to write academic paper

---
+++ papers/bib/papers-have-read/Clustering by fast search and find of density peaks_files/1163337475TopRight1Right2.js
@@ -0,0 +1,33 @@
+function OAS_RICH(position) {
+if (position == 'Right1') {
+document.write ('<div class="ad"><div STYLE="color: #52819B; font-size: 10px; text-align: center;" >ADVERTISEMENT</div><br />\n');
+document.write ('<a href="http://oascentral.sciencemag.org/RealMedia/ads/click_lx.ads/www.sciencemag.org/content/344/6191/1492.full/L26/1922643788/Right1/AAAS/SOL-Marketing-Nationwide-Member-Benefit-Button/V2-Extole-banner-160x150_Button_131016.html/435651302b3150463761344144653270?1922643788" target="_blank"><img src="http://imagec17.247realmedia.com/RealMedia/ads/Creatives/AAAS/SOL-Marketing-Nationwide-Member-Benefit-Button/V2-Extole-banner-160x150_Button_131016.jpg" BORDER=0 WIDTH=160 HEIGHT=150 alt="Advertisement"></a></div>\n');
+}
+if (position == 'Right2') {
+document.write ('<div class="ad"><div STYLE="color: #52819B; font-size: 10px; text-align: center;" >ADVERTISEMENT</div><br />\n');
+document.write ('\n');
+document.write ('<script type="text/javascript"><!--\n');
+document.write ('google_ad_client = "ca-pub-2288234982592986";\n');
+document.write ('google_ad_width = 160;\n');
+document.write ('google_ad_height = 600;\n');
+document.write ('//-->\n');
+document.write ('</script>\n');
+document.write ('<script type="text/javascript"\n');
+document.write ('src="//pagead2.googlesyndication.com/pagead/show_ads.js">\n');
+document.write ('</script>\n');
+document.write ('</div>\n');
+}
+if (position == 'Top') {
+document.write ('<script type="text/javascript"><!--\n');
+document.write ('google_ad_client = "ca-pub-2288234982592986";\n');
+document.write ('/* ROS_728x90 */\n');
+document.write ('google_ad_slot = "6288104001";\n');
+document.write ('google_ad_width = 728;\n');
+document.write ('google_ad_height = 90;\n');
+document.write ('//-->\n');
+document.write ('</script>\n');
+document.write ('<script type="text/javascript"\n');
+document.write ('src="//pagead2.googlesyndication.com/pagead/show_ads.js">\n');
+document.write ('</script>\n');
+}
+}
